7fa6606717b031292f4fedb320d961f09d587c6d
pengudata <pengudata@gmail.com>
2024-09-13 10:00:24 +0900

    Delete 2427-first-letter-to-appear-twice directory

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def repeatedCharacter(self, s: str) -> str:
-        d = defaultdict(int)
-        for char in s:
-            d[char] += 1
-            if d[char] == 2:
-                return char

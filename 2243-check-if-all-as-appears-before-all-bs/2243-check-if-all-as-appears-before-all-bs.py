08487b0927125bb39f4fddb397bd55bfef7638ec
pengudata <pengudata@gmail.com>
2024-09-19 09:55:26 +0900

    Delete 2243-check-if-all-as-appears-before-all-bs directory

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def checkString(self, s: str) -> bool:
-        found_b = False
-        for char in s:
-            if char == 'b':
-                found_b = True
-            if found_b and char == 'a':
-                return False
-        return True 

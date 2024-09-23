bdd0fb07d63b9fadfd6e8d3fa1f6d548c9ecbb98
pengudata <pengudata@gmail.com>
2024-09-23 09:43:06 +0900

    Delete 2163-kth-distinct-string-in-an-array directory

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def kthDistinct(self, arr: List[str], k: int) -> str:
-        count = Counter(arr)
-        candidates = []
-        for char, counts in count.items():
-            if counts == 1:
-                candidates.append(char)
-        return candidates[k-1] if k <= len(candidates) else ""

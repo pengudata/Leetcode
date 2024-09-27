b0ae43276ed36b84a7f8dc7a859dac43e28efe76
pengudata <pengudata@gmail.com>
2024-09-27 09:16:07 +0900

    Delete 2199-two-furthest-houses-with-different-colors directory

--- 2199-two-furthest-houses-with-different-colors/2199-two-furthest-houses-with-different-colors.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxDistance(self, colors: List[int]) -> int:
-        answer = 0
-        for i in range(len(colors)):
-            for j in range(len(colors)):
-                if colors[i] != colors[j]:
-                    answer = max(answer, abs(j - i))
-        return answer

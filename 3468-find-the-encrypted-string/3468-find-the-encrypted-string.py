c9288e45c04e643ca4675b8ca7bf26f7af4a9981
pengudata <pengudata@gmail.com>
2024-10-03 09:09:44 +0900

    Delete 3468-find-the-encrypted-string directory

--- 3468-find-the-encrypted-string/3468-find-the-encrypted-string.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def getEncryptedString(self, s: str, k: int) -> str:
-        length = len(s)
-        answer = ""
-        for i in range(length):
-            i = (i + k) % length
-            answer += s[i]
-        return answer

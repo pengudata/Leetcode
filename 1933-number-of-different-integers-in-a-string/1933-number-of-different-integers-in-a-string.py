ad0701b97c4b6b8c8d798ef3ae146d546fcdfa36
pengudata <pengudata@gmail.com>
2024-10-10 11:28:49 +0900

    Delete 1933-number-of-different-integers-in-a-string directory

--- 1933-number-of-different-integers-in-a-string/1933-number-of-different-integers-in-a-string.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def numDifferentIntegers(self, word: str) -> int:
-        num_word = re.findall('(\d+)', word)
-        nums = (int(x) for x in num_word)
-        return len(set(nums))
